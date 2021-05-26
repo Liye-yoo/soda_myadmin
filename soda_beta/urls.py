960c83d5f9e5dc04c08da777b8e3dc5168d3057d
liye <lyyoo38@gmail.com>
2021-05-27 07:08:59 +0900

    1. test`

--- soda_beta/urls.py
+++ soda_beta/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from soda_beta import views
+from soda_beta import views, _ajax
 
 app_name = 'soda_beta'
 urlpatterns = [
 
     path('qahelper/<slug:type>', views.QAHelperView.as_view(), name='qa_helper'),
+    path('<slug:app>/style', views.StyleView.as_view(), name='app_style'),
+    path('ajax/api/style', _ajax.call_style_api, name='api_style')
     ]
 
 # 메인 홈 > QAHelperView를 추가함. 이렇게 설정하는 방법들은 정규식으로도 할 수 있고,
